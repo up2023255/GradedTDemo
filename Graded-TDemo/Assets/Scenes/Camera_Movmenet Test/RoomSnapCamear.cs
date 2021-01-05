49d79176dacfb74a1d68b639645be3c875d286d5
up2023255 <luke_alexander_carr@hotmail.co.uk>
2021-01-05 17:18:17 +0000

    Snap Camera Testing
    Begun work on camera snapping from room to room

---
+++ Graded-TDemo/Assets/Scenes/Camera_Movmenet Test/RoomSnapCamear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSnapCamear : MonoBehaviour
+{
+    public Transform target;
+    
+    public float speed;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(11.75f, 0f, 0f);
+        }
+    }
+   /* public void moveCameraLeft ()
+    {
+        transform.Translate(12f, 0f, 0f);
+    }*/
+}
