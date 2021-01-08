b0ba307e4d4650a0e68abd66c81199c8d59c31c5
Bryce Reading <Bryce.uo@gmail.com>
2021-01-08 15:51:26 +0000

    Enemy AI scriped added not finished

---
+++ Graded-TDemo/Assets/Scenes/Bryce'sScene/BryceScrips/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State
+    {
+        Roaming,
+        ChaseTarget,
+    }
+
+    private Vector2 RandomPos;
+    private Vector2 StartPos;
+    private State state;
+    //public GameObject Player;
+
+    public static Vector2 RanDirection()
+    {
+        return new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
+    }
+    private void TargetFound()
+    {
+        float Range = 50f;
+        //if (Vector2.Distance(transform.position ) < Range) ;
+    }
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+}
