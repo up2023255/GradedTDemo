a633eb5983306f8981a4fb4f191b4e8b0ae5fb5e
GeneticGamer3165 <75086607+GeneticGamer3165@users.noreply.github.com>
2021-01-27 22:07:55 +0000

    Imorted animation

---
+++ Graded-TDemo/Assets/Scripts/combatConcept.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class combatConcept : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+        void Attack(){
+            anim.SetTrigger("Attack");
+        }
+}
