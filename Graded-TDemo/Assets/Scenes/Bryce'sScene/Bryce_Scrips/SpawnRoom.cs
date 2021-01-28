8bf5d9a0f9b12a39d2a1902242509cf214d8e7d5
Bryce Reading <Bryce.uo@gmail.com>
2021-01-28 16:04:39 +0000

    Map Gen works but still janky

--- Graded-TDemo/Assets/Scenes/Bryce'sScene/Bryce_Scrips/SpawnRoom.cs
+++ Graded-TDemo/Assets/Scenes/Bryce'sScene/Bryce_Scrips/SpawnRoom.cs
@@ -16,7 +16,6 @@ private
     {
         // Sets template to the list of rooms declared in "RoomTemplates"
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-
         Invoke("Spawn", 0.01f);
     }
 
@@ -24,44 +23,69 @@ private
     // Update is called once per frame
     void Spawn()
     {
-        switch(Opening)
+        if (Opening == 1)
         {
-            case 1:
-                    //A room with bottom door spawns
-                rand = Random.Range(0,templates.BottomRoom.Length);
+            //A room with bottom door spawnsx
+            rand = Random.Range(0,templates.BottomRoom.Length);
+            Instantiate(templates.BottomRoom[rand], transform.position, templates.BottomRoom[rand].transform.rotation);
+        }
+        else if (Opening == 2)
+        {
+            // A room with Top door spawns
+            rand = Random.Range(0,templates.TopRoom.Length);
+            Instantiate(templates.TopRoom[rand], transform.position, templates.TopRoom[rand].transform.rotation);
+        }
+        else if (Opening == 3)
+        {
+               // A room with left door spawns
+            rand = Random.Range(0,templates.LeftRoom.Length);
+            Instantiate(templates.LeftRoom[rand], transform.position, templates.LeftRoom[rand].transform.rotation);
+        }
+        else if (Opening == 4)
+        {
+            // A room with right door spawns
+            rand = Random.Range(0,templates.RightRoom.Length);
+            Instantiate(templates.RightRoom[rand], transform.position, templates.RightRoom[rand].transform.rotation);
 
-                Instantiate(templates.BottomRoom[rand], transform.position, templates.BottomRoom[rand].transform.rotation);
-
-                break;
-
-            case 2:
-                    // A room with Top door spawns
-                rand = Random.Range(0,templates.TopRoom.Length);
-
-                Instantiate(templates.TopRoom[rand], transform.position, templates.TopRoom[rand].transform.rotation);
-
-                break;
-
-            case 3:
-                   // A room with left door spawns
-                rand = Random.Range(0,templates.LeftRoom.Length);
-
-                Instantiate(templates.LeftRoom[rand], transform.position, templates.LeftRoom[rand].transform.rotation);
-
-                break;
-
-            case 4:
-                   // A room with right door spawns
-                rand = Random.Range(0,templates.RightRoom.Length);
-
-                Instantiate(templates.RightRoom[rand], transform.position, templates.RightRoom[rand].transform.rotation);
-
-                break;
         }
     }
 
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    Destroy(gameObject);
-    //}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
 }
+//switch(Opening)
+//{
+//    case 1:
+//            //A room with bottom door spawnsx
+//        rand = Random.Range(0,templates.BottomRoom.Length);
+
+//        Instantiate(templates.BottomRoom[rand], transform.position, templates.BottomRoom[rand].transform.rotation);
+
+//        break;
+
+//    case 2:
+//            // A room with Top door spawns
+//        rand = Random.Range(0,templates.TopRoom.Length);
+
+//        Instantiate(templates.TopRoom[rand], transform.position, templates.TopRoom[rand].transform.rotation);
+
+//        break;
+
+//    case 3:
+//           // A room with left door spawns
+//        rand = Random.Range(0,templates.LeftRoom.Length);
+
+//        Instantiate(templates.LeftRoom[rand], transform.position, templates.LeftRoom[rand].transform.rotation);
+
+//        break;
+
+//    case 4:
+//           // A room with right door spawns
+//        rand = Random.Range(0,templates.RightRoom.Length);
+
+//        Instantiate(templates.RightRoom[rand], transform.position, templates.RightRoom[rand].transform.rotation);
+
+//        break;
+//}
