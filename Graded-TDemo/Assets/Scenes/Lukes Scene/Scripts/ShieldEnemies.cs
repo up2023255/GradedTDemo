4d09df1521b41bd0456087b299516955f5303e7c
up2023255 <luke_alexander_carr@hotmail.co.uk>
2021-02-10 18:42:22 +0000

    Slow Knight Added - Working towards wall trap.

---
+++ Graded-TDemo/Assets/Scenes/Lukes Scene/Scripts/ShieldEnemies.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldEnemies : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("Inch", 1.0f, 1.0f);
+    }
+
+    void Update()
+    {
+        
+        //InvokeRepeating("Inch", 1.0f, 50.0f);
+    }
+
+    void Inch()
+    {
+        target = new Vector2(player.position.x, player.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2d(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           //DealDamage.
+        }
+    }
+
+}
