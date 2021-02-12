e84f74ed35287033c01ae2d0999d5d2859f3d37d
Bryce Reading <Bryce.uo@gmail.com>
2021-02-12 17:12:16 +0000

    Blood donation update
    Blood is now stored and will carry over runs. I still need to fully find a way of accsessing the players blood and then adding it ti the current total donated.
    The UI text floats above the alter.

--- Graded-TDemo/Assets/Scenes/Bryce'sScene/Bryce_Scrips/BloodDonated.cs
+++ Graded-TDemo/Assets/Scenes/Bryce'sScene/Bryce_Scrips/BloodDonated.cs
@@ -5,17 +5,27 @@ using UnityEngine.UI;
 
 public class BloodDonated : MonoBehaviour
 {
+    int Blood;
     public Text bloodText;
-    Player bloodDonate;
+    // Player player;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Accesses the player script and links the health variable.
+        Blood = player.GetComponent<Player>().maxHealth;
+
+        //Takes the saved data and displayes it on the screen for the player.
+        bloodText.text = PlayerPrefs.GetInt("Blood Donated").ToString() + Blood;
+        //bloodText.text = "Blood Donated: ";
     }
 
     // Update is called once per frame
     void Update()
     {
-        bloodText.text = "Blood Donated:" + bloodDonate.maxHealth;
+        //Takes the players current health/blood and stores it on the system to allow it to remain over many runs.
+        PlayerPrefs.SetInt("Blood Donated", Blood);
+
     }
 }
