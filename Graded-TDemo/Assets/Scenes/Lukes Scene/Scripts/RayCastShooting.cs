f4cf4b1be14cd86fe24b7265f58d935a12c1b252
up2023255 <luke_alexander_carr@hotmail.co.uk>
2021-02-16 15:04:26 +0000

    RayCast Enemies Testing

---
+++ Graded-TDemo/Assets/Scenes/Lukes Scene/Scripts/RayCastShooting.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using CodeMonkey.Utils;
+
+public class RayCastShooting : MonoBehaviour
+{
+
+    public float damage = 100f;
+    public float range = 100f;
+    public GameObject endPosition;
+
+    [SerializeField] private Material rayMaterial;
+
+    void Start()
+    {
+        endPosition = GameObject.Find("EndPosition");
+    }
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            CreateRay(this.gameObject.transform.position, endPosition.transform.position);
+            Debug.Log("Hi");
+            Fire();
+        }
+    }
+
+    void Fire ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(this.gameObject.transform.position, this.gameObject.transform.right, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            
+        }
+
+    }
+
+    /*public void CreateRay (Vector3 fromPosition, Vector3 targetPosition)
+    {
+        Vector3 dir = (targetPosition - fromPosition).normalized;
+        float eulerZ = UtilsClass.GetAngleFromVectorFloat(dir) -180;
+        float distance = Vector3.Distance(fromPosition, targetPosition);
+        Vector3 tracerSpawnPosition = fromPosition + dir * distance * .4f;
+        World_Mesh worldMesh = World_Mesh.Create(tracerSpawnPosition, eulerZ, 6f, distance, rayMaterial, null, 10000);
+    }*/
+}
